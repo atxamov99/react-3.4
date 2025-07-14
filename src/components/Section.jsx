69c37de4445634df213d48e7e3e8ef6de694f7d6
atxamov99 <atxamov99@inbox.ru>
2025-07-14 20:05:11 +0500

    sadoin

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -16,14 +16,7 @@ const Section = () => {
           <p>Lavender, with its enchanting aroma and rich history, <br /> has been cherished for centuries as a symbol of <br /> relaxation, healing, and timeless beauty. In the world <br /> of perfumery, lavender plays a key role in creating captivating <br /> fragrances loved by many.</p>
           <button className='bg-none border-2 border-white rounded-2xl p-[10px] w-[150px]'>Read More</button>
         </div>
-
-        <div className="flex flex-col space-y-8">
-          <img src={Soothing} alt="" className='w-[400px] h-[400px] object-cover rounded-lg' />
-          <h3 className='font-bold'>The Art of Curating a Luxury Perfume <br /> Collection: A Symphony of Scents and <br /> Stories</h3>
-          <p>A luxury perfume collection is not just an assortment <br /> of fragrances; it is a reflection of one's taste, <br /> personality, and experiences. Each bottle holds a <br /> unique olfactory journey, crafted with the finest <br /> ingredients and artistic mastery.</p>
-          <button className='bg-none border-2 border-white rounded-2xl p-[10px] w-[150px]'>Read More</button>
-        </div>
-
+        
         <div className="flex flex-col space-y-8">
           <img src={Unveiling} alt="" className='w-[400px] h-[400px] object-cover rounded-lg' />
           <h3 className='font-bold'>The Timeless Elegance of Rose <br /> Perfumes: Unveiling the Queen of <br /> Flowers in Fragrance</h3>
