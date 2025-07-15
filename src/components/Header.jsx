244d66338c322a3b2226a02c19c1bede6de9989f
Wahrizoda2007 <shahrizodaabdurahmonova708@gmail.com>
2025-07-15 18:39:14 +0500

    scvds

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import { FaSearch, FaUser, FaHeart, FaShoppingBag } from 'react-icons/fa';
 const Header = () => {
   return (
     <div className="bg-black text-white">
-      {/* Header top section */}
+      
       <div className="flex justify-between items-center px-8 py-5">
-        {/* Logo */}
+       
         <h1 className="text-2xl font-bold bg-gradient-to-r from-orange-500 to-yellow-300 text-transparent bg-clip-text">
           Local Face
         </h1>
 
-        {/* Navigation menu */}
+       
         <nav className="hidden md:flex space-x-6 text-sm font-medium">
           <a href="#" className="hover:text-orange-400">Home</a>
           <a href="#" className="hover:text-orange-400">Shop</a>
@@ -20,7 +20,7 @@ const Header = () => {
           <a href="#" className="hover:text-orange-400">Blog</a>
         </nav>
 
-        {/* Icons */}
+       
         <div className="flex space-x-6 text-lg">
           <FaSearch className="cursor-pointer hover:text-orange-400" />
           <FaUser className="cursor-pointer hover:text-orange-400" />
@@ -29,7 +29,7 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Breadcrumbs */}
+     
       <div className="px-8 pb-4 text-sm text-gray-400">
         Home / <span className="text-white">Products</span>
       </div>
