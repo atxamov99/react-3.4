950251e09409f4a47877f199752f05cc8776d7a3
Wahrizoda2007 <shahrizodaabdurahmonova708@gmail.com>
2025-07-15 18:44:06 +0500

    fsfsdfd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
 import Header from './Header';
 import FilterBar from './FilterBar';
 import ProductList from './ProductList';
 import SpecialOffers from './SpecialOffers';
 import Footer from './Footer';
+=======
+import Header from './components/Header';git push
+import FilterBar from './components/FilterBar';
+import ProductList from './components/ProductList'; 
+import SpecialOffers from './components/SpecialOffers';
+import Footer from './components/Footer';
+>>>>>>> 270ce3b (O'zgarishlar commit qilindi)
 
 function Home() {
   return (
